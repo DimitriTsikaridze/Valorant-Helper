9a9fef2d4a517c2a6b68054a8920773f1a74e613
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2022-02-24 21:43:19 +0400

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, MainComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'valorant-helper-fca81',
+    appId: '1:845053606056:web:d70841d8be4edba6b66a9d',
+    storageBucket: 'valorant-helper-fca81.appspot.com',
+    apiKey: 'AIzaSyDt6dSYCsGpQIZ8PKQxWiRBxdQ1mLe9IQo',
+    authDomain: 'valorant-helper-fca81.firebaseapp.com',
+    messagingSenderId: '845053606056',
+  },
   production: true
 };

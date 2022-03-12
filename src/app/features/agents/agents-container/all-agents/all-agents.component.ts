3fb4b5df581be9363bc99f743613fb9a261b0ba6
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2022-03-12 14:23:32 +0400

    feat: add agent role filter

--- src/app/features/agents/agents-container/all-agents/all-agents.component.ts
+++ src/app/features/agents/agents-container/all-agents/all-agents.component.ts
@@ -2,6 +2,7 @@ import {
   ChangeDetectionStrategy,
   Component,
   Input,
+  OnChanges,
   OnInit,
 } from '@angular/core';
 import { Router } from '@angular/router';
@@ -13,16 +14,30 @@ import { Agent } from '../../../../shared/models/agent.interface';
   styleUrls: ['./all-agents.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AllAgentsComponent implements OnInit {
+export class AllAgentsComponent implements OnInit, OnChanges {
   constructor(private router: Router) {}
 
   @Input() agents!: Agent[];
+  tempAgents!: Agent[];
 
   ngOnInit(): void {
     window.scrollTo(0, 0);
   }
 
+  ngOnChanges(): void {
+    this.tempAgents = this.agents;
+  }
+
   onAgentClick(pathName: string) {
     this.router.navigate(['agents', pathName]);
+  }
+
+  roleClick(value: HTMLLIElement) {
+    const clickedRole = value.innerText.toLowerCase();
+    const filteredAgents = this.tempAgents.filter((agent) => {
+      const agentRole = agent.role.displayName.toLowerCase();
+      return agentRole == clickedRole;
+    });
+    this.agents = filteredAgents;
   }
 }

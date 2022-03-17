7408074e6f528a8b5a5e07a84153454fc7b73e81
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2022-03-17 23:46:10 +0400

    feat: add hamburger menu

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -5,4 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  active = false;
+  menuIcon = 'https://i.ibb.co/HrfVRcx/menu.png';
+  closeIcon = 'https://i.ibb.co/rt3HybH/close.png';
+  iconURL = this.menuIcon;
+
+  changeIcon() {
+    this.active = !this.active;
+    this.active
+      ? (this.iconURL = this.closeIcon)
+      : (this.iconURL = this.menuIcon);
+  }
+}

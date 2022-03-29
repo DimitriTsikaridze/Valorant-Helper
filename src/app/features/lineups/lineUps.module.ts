7994f899a2b29ca9a023f414d69887afaa191b83
Dimitri Tsikaridze <dimitri.tsikaridze.1@btu.edu.ge>
2022-03-29 10:17:10 +0400

    add crosshair submit link

--- src/app/features/lineups/lineUps.module.ts
+++ src/app/features/lineups/lineUps.module.ts
@@ -7,9 +7,16 @@ import { SharedModule } from '../../shared/shared.module';
 import { AgentLineupsPreviewComponent } from './lineups-container/agent-lineups-preview/agent-lineups-preview.component';
 import { AgentLineupMapsComponent } from './lineups-container/agent-lineup-maps/agent-lineup-maps.component';
 import { MapPreviewComponent } from './lineups-container/map-preview/map-preview.component';
+import { ToggleSitesDirective } from './toggle-sites.directive';
 
 @NgModule({
-  declarations: [LineupsContainerComponent, AgentLineupsPreviewComponent, AgentLineupMapsComponent, MapPreviewComponent],
+  declarations: [
+    LineupsContainerComponent,
+    AgentLineupsPreviewComponent,
+    AgentLineupMapsComponent,
+    MapPreviewComponent,
+    ToggleSitesDirective,
+  ],
   imports: [CommonModule, LineupsRoutingModule, SharedModule],
 })
 export class LineupsModule {}

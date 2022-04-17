fd4d74c8598803f84412231677582de446b52aee
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2022-04-17 10:43:22 +0400

    refactor: move component template and styles inline

--- src/app/features/error/error.component.ts
+++ src/app/features/error/error.component.ts
@@ -2,8 +2,22 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-error',
-  templateUrl: './error.component.html',
-  styleUrls: ['./error.component.scss'],
+  template: `<h1>Page Not Found</h1> `,
+  styles: [
+    `
+      @use '/src/styles/index' as *;
+      :host {
+        display: block;
+        min-height: 100%;
+        background-color: black;
+      }
+
+      h1 {
+        text-align: center;
+        padding-block: 2rem;
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ErrorComponent {}

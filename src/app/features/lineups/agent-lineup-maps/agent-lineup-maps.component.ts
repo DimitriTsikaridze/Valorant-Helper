0a3cc5001c491269ec58d2b17599718e226ed742
Dimitri <dimitri.tsikaridze.1@btu.edu.ge>
2022-08-05 22:12:34 +0400

    add title for single agent lineups

--- src/app/features/lineups/agent-lineup-maps/agent-lineup-maps.component.ts
+++ src/app/features/lineups/agent-lineup-maps/agent-lineup-maps.component.ts
@@ -1,4 +1,5 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LineupsService } from '@services/lineups.service';
 
@@ -8,16 +9,27 @@ import { LineupsService } from '@services/lineups.service';
   styleUrls: ['./agent-lineup-maps.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AgentLineupMapsComponent {
+export class AgentLineupMapsComponent implements OnInit {
   constructor(
     private lineupsService: LineupsService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private title: Title
   ) {}
 
   maps$ = this.lineupsService.maps$;
 
   mockSites = ['A', 'B'];
+
+  agentName: string = this.route.snapshot.params['agentName'];
+
+  ngOnInit() {
+    this.title.setTitle(
+      `${
+        this.agentName.charAt(0).toUpperCase() + this.agentName.slice(1)
+      } Lineups`
+    );
+  }
 
   navigateToLineup(mapName: string, siteName: string) {
     this.router.navigate([mapName, siteName], { relativeTo: this.route });

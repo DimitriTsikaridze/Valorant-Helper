67e888e708269590bd1627165519a8b9cfe3ecfa
Dimitri Tsikaridze <dimitri.tsikaridze.1@btu.edu.ge>
2022-08-20 23:33:28 +0400

    refactor: add prettier config file and plugin for organizing angularcomponent attributes

--- src/app/features/crosshairs/crosshairs-container/crosshairs-container.component.ts
+++ src/app/features/crosshairs/crosshairs-container/crosshairs-container.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-crosshairs-container',
@@ -6,4 +6,4 @@ import { ChangeDetectionStrategy, Component, } from '@angular/core';
   styleUrls: ['./crosshairs-container.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CrosshairsContainerComponent  {}
+export class CrosshairsContainerComponent {}

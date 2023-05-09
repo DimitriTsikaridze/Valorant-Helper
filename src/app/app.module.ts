c725b5cd293a1ad0184e429ead1655b04168c8a6
DimitriTsikaridze <dimitri.tsikaridze.1@btu.edu.ge>
2023-05-08 21:15:04 -0700

    refactor: remove old pwa files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { ServiceWorkerModule } from '@angular/service-worker';
 import { LayoutModule } from './layout/layout.module';
 
 import { AppComponent } from './app.component';
 
-import { environment } from '@environment/environment';
 import { ErrorHandlerInterceptor } from '@shared/interceptors';
 
 @NgModule({
@@ -17,10 +15,6 @@ import { ErrorHandlerInterceptor } from '@shared/interceptors';
     AppRoutingModule,
     HttpClientModule,
     LayoutModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      registrationStrategy: 'registerWhenStable:30000',
-    }),
   ],
   providers: [
     {

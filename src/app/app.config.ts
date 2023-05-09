1c0a4baa6379484ede875a811876a538a7ec5e4c
DimitriTsikaridze <dimitri.tsikaridze.1@btu.edu.ge>
2023-05-08 23:04:07 -0700

    feat: add pwa support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,26 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import {
   provideRouter,
-  withDisabledInitialNavigation,
+  withEnabledBlockingInitialNavigation,
   withInMemoryScrolling,
 } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorHandlerInterceptor } from '@shared/interceptors';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(
       routes,
-      // withDisabledInitialNavigation(),
-      withInMemoryScrolling()
+      withEnabledBlockingInitialNavigation(),
+      withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })
     ),
     provideHttpClient(withInterceptors([errorHandlerInterceptor])),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };

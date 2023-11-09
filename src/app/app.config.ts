9c043d71a7429aaca96f842b8d5736a0b49ee905
DimitriTsikaridze <dimitri.tsikaridze.1@btu.edu.ge>
2023-11-09 12:05:46 +0400

    change scss variables to css custom properties

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { errorHandlerInterceptor } from '@shared/interceptors';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(withInterceptors([errorHandlerInterceptor])),
+    provideHttpClient(withInterceptors([errorHandlerInterceptor]), withFetch()),
     provideClientHydration()
   ],
 };

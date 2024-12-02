b675d66908d71601f066b0f6391b6677d4b98ddb
DimitriTsikaridze <dtsikaridze@proton.me>
2024-12-02 18:16:12 +0400

    remove unused imports and console warnings

--- src/app/features/agents/agent-news/agent-news.component.ts
+++ src/app/features/agents/agent-news/agent-news.component.ts
@@ -1,14 +1,14 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ButtonComponent } from '@shared/components';
-import { NgStyle, AsyncPipe } from '@angular/common';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-agent-news',
   templateUrl: './agent-news.component.html',
   styleUrls: ['./agent-news.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [NgStyle, ButtonComponent, RouterLink, AsyncPipe],
+  imports: [NgStyle, ButtonComponent, RouterLink],
 })
 export class AgentNewsComponent {
   agent = inject(ActivatedRoute).snapshot.data['newAgent'];
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseUrl: 'https://raw.githubusercontent.com/DimitriTsikaridze/Valorant-Helper-API/main/',
-  appVersion: '1.0.0',
+  appVersion: '1.0.1',
 };
